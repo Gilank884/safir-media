cfc2a7bf3f0f0f9764fbd811f5e3ae16a32fc9ce
GILANK884 <gilankprasetyo8@gmail.com>
2025-06-28 18:01:22 +0800

    Fix build: update to use output: export

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
-export default {
-  output: 'export',
-  images: {
-    unoptimized: true
-  }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // ⬅️ ini bagian penting
 };
+
+export default nextConfig;
