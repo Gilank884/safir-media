0e84bc9bed9b53b04d1f000bd0c0992eb7c4735a
GILANK884 <gilankprasetyo8@gmail.com>
2025-06-29 15:48:04 +0800

    Update semua perubahan

--- src/components/Landing/index.jsx
+++ src/components/Landing/index.jsx
@@ -45,7 +45,7 @@ export default function Home() {
   return (
     <motion.main variants={slideUp} initial="initial" animate="enter" className={styles.landing}>
       <Image
-        src="/images/background1.jpeg"
+        src="/images/utama.jpg"
         fill={true}
         alt="background"
         className="brightness-50"
